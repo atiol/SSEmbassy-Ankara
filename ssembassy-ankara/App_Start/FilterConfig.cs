f813160e6e1082f35c669aa306b841e1eeb1e467
HP7 <HP7@HP7-HP>
2018-06-27 13:09:33 +0300

    Add project files.

---
+++ ssembassy-ankara/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ssembassy_ankara
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
