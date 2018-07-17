fa3e7e956cc5adc949502518acc90bafb8f304ef
Atiol <atraf90.ar@gmail.com>
2018-07-17 14:05:43 +0300

    restored login successfully

---
+++ ssembassy-ankara/Controllers/MainLayoutPartialsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ssembassy_ankara.Models;
+
+namespace ssembassy_ankara.Controllers
+{
+    public class MainLayoutPartialsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MainLayoutPartialsController()
+        {
+            // Initialize db context
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Welcome message
+        public PartialViewResult _WelcomeMessagePartialViewResult()
+        {
+            var model = _context.WelcomeMessage.FirstOrDefault();
+            
+            return PartialView( new WelcomeMessage()
+            {
+                Message = model.Message
+            });
+        }
+
+        // Get: Ambassador Info
+
+        
+        // GET: Address Info
+        public PartialViewResult _EmbassyAddressPartialViewResult()
+        {
+            var model = _context.EmbassyAddress.FirstOrDefault();
+            return PartialView(new embassy_address()
+            {
+                address = model.address,
+                telephone = model.telephone,
+                email = model.email,
+                fax = model.fax
+            });
+        }
+    }
+}
---
+++ ssembassy-ankara/Models/AmbassadorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ssembassy_ankara.Models
+{
+    public class AmbassadorViewModel : ApplicationUser
+    {
+        public string Bio { get; set; }
+        public string Message { get; set; }
+    }
+}
